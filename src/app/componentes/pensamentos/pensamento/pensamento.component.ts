035cf00938d56e6e5ba620fbaac6ceb5c05d1d85
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-12-22 15:53:07 -0300

    Create pensamento.component

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  @Input() pensamento = {
+    conteudo: 'I Love Angular',
+    autoria: 'Samuel',
+    modelo: 'modelo3'
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
